e45ffab4950810e1433e52e6ebc18e73ec97b013
AymenRumi <aymen.rumi@mail.mcgill.ca>
2023-12-14 16:47:54 -0500

    ec2 setup

--- scripts/ec2-setup.sh
+++ scripts/ec2-setup.sh
@@ -1,36 +1,21 @@
 #!/bin/bash
 
 
-
-# sudo yum install -y python3-pip
-# pip3 install remax-pipeline
-
-echo "Hello" >> testfile.txt
 sudo wget https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip
 sudo unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin/chromedriverchromedriver --version
 
 sudo curl -k https://intoli.com/install-google-chrome.sh | bash
 sudo mv /usr/bin/google-chrome-stable /usr/bin/google-chrome
-sudo google-chrome --version && which google-chrome >> chromefile.txt
+sudo google-chrome --version && which google-chrome 
 
 
-which pip3 >> t.txt
 
 sudo yum install -y python3-pip
 
 
 
-which pip3 >> whichpip.txt
-
-pwd >> pwd.txt
-
-# pip3 install celery > install_log.txt 2>&1
-
-pip3 install --ignore-installed remax-pipeline > remax_install_log.txt 2>&1
+pip3 install --ignore-installed remax-pipeline
 
 
-pip3 freeze > requirements.txt
 
-
-celery >> celery.txt
