e67bbf0c8f98bec92c331445890213ea785d8203
AymenRumi <aymen.rumi@mail.mcgill.ca>
2023-12-18 23:27:19 -0500

    dockerfile

--- scripts/rabbitmq-ecr-image.sh
+++ scripts/rabbitmq-ecr-image.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 export AWS_SECRETS=remax-pipeline-secrets
 export IMAGE_NAME=rabbitmq
 export ECR_REGISTRY=$(aws secretsmanager get-secret-value --secret-id ${AWS_SECRETS} --query 'SecretString' --output text | jq -r '.repository_url')
