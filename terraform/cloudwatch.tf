1ac6b64e691b463e4524b635cb32a57a892488e4
AymenRumi <aymen.rumi@mail.mcgill.ca>
2023-12-30 17:07:57 -0500

    cloudwatch

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -4,16 +4,16 @@ resource "aws_cloudwatch_event_rule" "every_minute_between_1_and_2_pm" {
   schedule_expression = "cron(0/1 * * * ? *)" 
 }
 
-# resource "aws_cloudwatch_event_target" "trigger_lambda" {
-#   rule      = aws_cloudwatch_event_rule.every_minute_between_1_and_2_pm.name
-#   target_id = "TriggerLambdaFunction"
-#   arn       = aws_lambda_function.rabbitmq_monitor.arn
-# }
+resource "aws_cloudwatch_event_target" "trigger_lambda" {
+  rule      = aws_cloudwatch_event_rule.every_minute_between_1_and_2_pm.name
+  target_id = "TriggerLambdaFunction"
+  arn       = aws_lambda_function.rabbitmq_monitor.arn
+}
 
-# resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda" {
-#   statement_id  = "AllowExecutionFromCloudWatch"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.rabbitmq_monitor.function_name
-#   principal     = "events.amazonaws.com"
-#   source_arn    = aws_cloudwatch_event_rule.every_minute_between_1_and_2_pm.arn
-# }
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.rabbitmq_monitor.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.every_minute_between_1_and_2_pm.arn
+}
