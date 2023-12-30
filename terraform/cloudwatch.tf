8c1f25ee20726e3dca248057482b9699901b7bd1
AymenRumi <aymen.rumi@mail.mcgill.ca>
2023-12-30 17:11:50 -0500

    lambda function name changed

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -7,13 +7,13 @@ resource "aws_cloudwatch_event_rule" "every_minute_between_1_and_2_pm" {
 resource "aws_cloudwatch_event_target" "trigger_lambda" {
   rule      = aws_cloudwatch_event_rule.every_minute_between_1_and_2_pm.name
   target_id = "TriggerLambdaFunction"
-  arn       = aws_lambda_function.rabbitmq_monitor.arn
+  arn       = aws_lambda_function.rabbitmq_autoscale.arn
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.rabbitmq_monitor.function_name
+  function_name = aws_lambda_function.rabbitmq_autoscale.function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.every_minute_between_1_and_2_pm.arn
 }
